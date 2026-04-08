38c360a0624d3f6a0dcc2b0c702ad75f7e89f254
adn <a.nur.kholis@students.amikom.ac.id>
2026-04-08 14:37:17 +0700

    update tugas

---
+++ calculator2.cs
@@ -0,0 +1,18 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}, a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}, a, b, Pengurangan(a, b));
+		
+		Console.ReadLine("\nTekan sembarang key untuk keluar");
+		Console.Readkey();
+	}
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+}
